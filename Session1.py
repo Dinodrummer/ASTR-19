59d25db1e6d0f5ceca4f5f70db56bae588bd7f60
JT Gruber <jtgruber12@gmail.com>
2026-04-14 13:02:14 -0700

    Session1

---
+++ Session1.py
@@ -0,0 +1,10 @@
+def main():
+    print("My name is JT Gruber (he/him). My favorite movie is probably Up, " \
+    "although more recent films have been close to taking its place. I just love " \
+    "the symbolism throughout the whole movie; while it seems like a fun movie, " \
+    "there are a lot of overarching themes that make it darker the more " \
+    "you look into it. A food that I like is ramen, as there are so many different" \
+    " things you can do with it.")
+
+if __name__ == '__main__':
+    main()
