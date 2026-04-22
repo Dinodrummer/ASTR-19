b39e0f5971b79663c835220c87dac141610b19fc
JT Gruber <jtgruber12@gmail.com>
2026-04-21 19:32:10 -0700

    Session4_1

---
+++ Session4.py
@@ -0,0 +1,24 @@
+class Animal:
+    def __init__(self, armLength, legLength, eyesCount, tail, furry):
+        self.armLength = armLength
+        self.legLength = legLength
+        self.eyesCount = eyesCount
+        self.tail = tail
+        self.furry = furry
+    def man(self):
+        print(f"armLength: {self.armLength} - Length of arms (FLOAT)")
+        print(f"legLength: {self.legLength} - Length of arms (FLOAT)")
+        print(f"eyesCount: {self.eyesCount} - Length of arms (INT)")
+        print(f"tail: {self.tail} - Length of arms (BOOL)")
+        print(f"furry: {self.furry} - Length of arms (BOOL)")
+
+
+    
+
+
+def main():
+    
+
+
+if __name__ == '__main__':
+    main()
